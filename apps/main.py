bea6ac4548b6a55ef8cb8d298d63e1a29c82c32e
anzilmo <anzilmo15@gmail.com>
2026-01-14 18:41:53 +0300

    redy to backend

--- apps/main.py
+++ apps/main.py
@@ -1,14 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 from apps.mongodb import users_collection
-
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[" http://localhost:5173/"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -16,13 +15,26 @@ app.add_middleware(
 
 @app.get("/home")
 def home():
-        return {
-        "title": "Welcome to the FastAPI Application",
-        "description": "This is a sample FastAPI application with CORS enabled.",
+    user = users_collection.find_one({}, {"_id": 0})
+    return {
+        "title": "FastAPI + React",
+        "message": f"MongoDB connected for {user['name']}" if user else "MongoDB is empty"
     }
 
+class UserIn(BaseModel):
+    name: str
 
-@app.get("/")
-def read_root():
-    user = users_collection.find_one({}, {"_id": 0})
-    return {"user": user}
+class UserOut(BaseModel):
+    id: str
+    name: str
+
+@app.post("/user", response_model=UserOut, status_code=status.HTTP_201_CREATED)
+def create_user(user: UserIn):
+    result = users_collection.insert_one({
+        "name": user.name
+    })
+
+    return {
+        "id": str(result.inserted_id),
+        "name": user.name
+    }
