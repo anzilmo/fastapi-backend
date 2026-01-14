a4677bb93ff31b93a16643ab90693e6a4827a148
anzilmo <anzilmo15@gmail.com>
2026-01-14 12:40:01 +0300

    fast api coneting MongoDB

--- apps/main.py
+++ apps/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from apps.mongodb import users_collection
+
 
 
 app = FastAPI()
@@ -20,3 +22,7 @@ def home():
     }
 
 
+@app.get("/")
+def read_root():
+    user = users_collection.find_one({}, {"_id": 0})
+    return {"user": user}
